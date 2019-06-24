00ae4cd0ba27f60bcafb9a597f7050e8d29d48ac
LinSundy <403514753@qq.com>
2019-06-25 07:44:13 +0800

    优化登录

--- app01/models.py
+++ app01/models.py
@@ -14,6 +14,5 @@ class UserInfo(models.Model):
 
 
 class UserToken(models.Model):
-    # user = models.OneToOneField(to='UserInfo', on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(to='UserInfo', on_delete=models.DO_NOTHING)
     token = models.CharField(max_length=64)
-    userName = models.CharField(max_length=32)
--- app01/views.py
+++ app01/views.py
@@ -16,29 +16,25 @@ def md5(s):
 
 
 class Login(APIView):
-    ret = {
-        'code': '0000',
-        'msg': '请求成功',
-        'token': ''
-    }
 
     def post(self, request, *args, **kwargs):
+        ret = {
+            'code': '0000',
+            'msg': None,
+        }
         user = request._request.POST.get('username')
         pwd = request._request.POST.get('password')
         try:
             user_obj = UserInfo.objects.get(username=user)
-            print(id(self.ret), 'id地址')
             if pwd == user_obj.password:
                 token = md5(user)
-                UserToken.objects.update_or_create(defaults={'userName': user, 'token': token})
-                self.ret['token'] = token
-                self.ret['msg'] = '请求成功'
+                UserToken.objects.update_or_create(user=user_obj, defaults={'token': token})
+                ret['token'] = token
+                ret['msg'] = '请求成功'
             else:
-                self.ret['msg'] = '用户名密码不正确'
-                self.ret['token'] = ''
-            return JsonResponse(self.ret)
+                ret['msg'] = '用户名密码不正确'
+            return JsonResponse(ret)
         except UserInfo.DoesNotExist:
-            self.ret['code'] = '4000'
-            self.ret['msg'] = '当前用户名不存在！'
-            self.ret['token'] = ''
-            return JsonResponse(self.ret)
+            ret['code'] = '4000'
+            ret['msg'] = '当前用户名不存在！'
+            return JsonResponse(ret)
